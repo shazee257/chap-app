e4f2a2f616a10358496226d4c7c507efb85f2978
Shahzad <shahzad@four-fox.com>
2023-06-27 02:18:04 +0500

    fixed

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -2,10 +2,13 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { fetchApi } from '@/fetchApi';
+import moment from 'moment';
 
 const chat = () => {
     const [fullName, setFullName] = useState("");
     const [userImage, setUserImage] = useState("");
+    const [messages, setMessages] = useState([] as any[]);
+    const userId = () => localStorage.getItem('userId') || "";
 
     type User = {
         _id: string;
@@ -17,9 +20,7 @@ const chat = () => {
     const [users, setUsers] = useState([] as User[]);
     const router = useRouter();
 
-    const getToken = () => {
-        return localStorage.getItem('token');
-    }
+    const getToken = () => localStorage.getItem('token');
 
     const logout = () => {
         localStorage.removeItem('userId');
@@ -49,10 +50,31 @@ const chat = () => {
         setUsers(data.data);
     }
 
+    const getMessages = async (userId: string) => {
+        const token = getToken();
+
+        const [data, error] = await fetchApi({
+            method: 'GET',
+            token,
+            // endPoint: `message/${userId}`,
+            // 6494a981c3b593ec06f243d3
+            endPoint: `message/6494ab5998dc9cbe75155f4b`,
+        });
+
+        if (error) {
+            console.error(error.message);
+            return;
+        }
+
+        console.log("data?.data >>>> ", data?.data);
+        setMessages(data?.data);
+    }
+
     useEffect(() => {
         getUsers();
         setFullName(localStorage.getItem('fullName') || "");
         setUserImage(localStorage.getItem('image') || "");
+        getMessages('sdfsdf');
     }, [])
 
     return (
@@ -81,12 +103,13 @@ const chat = () => {
                 <div className="flex flex-col w-3/12 border-r-2 border-white">
                     <h1 className="text-2xl font-semibold">Users List</h1>
                     {users.map((user, index) => (
-                        <div key={index} className="flex items-center space-x-4 my-4 cursor-pointer">
+                        <div key={index} className="flex items-center space-x-4 my-4 cursor-pointer"
+                            onClick={() => getMessages(user._id)}>
                             <Image
                                 src={`${process.env.NEXT_PUBLIC_BaseURL}/uploads/${user.image}`}
-                                width={48}
-                                height={48}
-                                alt="user" className="w-12 h-12 rounded-full" />
+                                width={36}
+                                height={36}
+                                alt="user" className="w-8 h-8 rounded-full" />
                             <h1 className="text-2xl font-semibold">{user.fullName}</h1>
                         </div>
                     ))}
@@ -98,8 +121,23 @@ const chat = () => {
                     <div className="flex flex-col w-full h-96 border-2 border-white rounded-md">
                         <div className="flex flex-col w-full h-5/6 overflow-y-scroll">
                             <div className="flex flex-col w-full h-1/6">
-                                <h1 className="text-white">Hello</h1>
-
+                                <div className="text-white p-4">
+                                    {messages.map((message) => (
+                                        <div key={message._id} className="flex flex-col items-start p-2 mb-2">
+                                            <div className="flex flex-col items-center">
+                                                <div className='flex space-x-4 py-1'>
+                                                    <Image
+                                                        src={`${process.env.NEXT_PUBLIC_BaseURL}/uploads/${message?.sender?.image}`}
+                                                        width={36}
+                                                        height={36}
+                                                        alt="user" className="w-8 h-8 rounded-full" />
+                                                    <div className="text-sm p-2 bg-blue-300 rounded-tr-xl rounded-br-xl rounded-bl-xl">{message.text}</div>
+                                                </div>
+                                                <div className='text-right text-gray-400 text-xs w-full'>{moment(message?.createdAt).format('h:mm:ss a')}</div>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
                         </div>
                     </div>
